c74552f95e21a7c8a21b97c0b2dff29221872694
Simon Hoiberg <simonlhoiberg@gmail.com>
2024-06-30 19:15:07 +0200

    refactor: improve menu component

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -9,10 +9,10 @@ export interface MenuProps {
 
 export const Menu: FC<MenuProps> = ({ page }) => {
   return (
-    <div className='flex bg-[#2040a8] rounded-lg p-2'>
+    <div className='flex bg-[#2040a8] rounded-lg p-2 max-w-full'>
       <Link href='/'>
         <button
-          className={`flex items-center p-2 mr-2 rounded-md focus:outline-none  ${
+          className={`flex items-center p-2 mr-2 rounded-md focus:outline-none shrink-0 ${
             page === 'chat' ? 'bg-[#2a4dc1] text-white' : 'hover:bg-[#2a4dc1] text-gray-200'
           }`}
         >
@@ -25,7 +25,7 @@ export const Menu: FC<MenuProps> = ({ page }) => {
 
       <Link href='/about'>
         <button
-          className={`flex items-center p-2 rounded-md focus:outline-none  ${
+          className={`flex items-center p-2 mr-2 rounded-md focus:outline-none shrink-0 ${
             page === 'about' ? 'bg-[#2a4dc1] text-white' : 'hover:bg-[#2a4dc1] text-gray-200'
           }`}
         >
@@ -36,12 +36,16 @@ export const Menu: FC<MenuProps> = ({ page }) => {
         </button>
       </Link>
 
-      <a href='https://github.com/Aidbase/ai-chat-website' target='_blank'>
-        <button className='flex items-center p-2 rounded-md focus:outline-none hover:bg-[#2a4dc1] text-gray-200 truncate'>
-          <FaGithub className='mr-1 text-gray-200' />
-          <span className='text-gray-200 text-ellipsis'>Use template</span>
-        </button>
-      </a>
+      <button className=' p-2 rounded-md focus:outline-none hover:bg-[#2a4dc1] text-gray-200 truncate'>
+        <a
+          className='flex items-center'
+          href='https://github.com/Aidbase/ai-chat-website'
+          target='_blank'
+        >
+          <FaGithub className='mr-1 shrink-0 text-gray-200' />
+          <span className='text-gray-200 truncate'>Use template</span>
+        </a>
+      </button>
     </div>
   );
 };
